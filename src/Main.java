0a06f309cc5929c33165550f3a3f1b0c102e7aaa
SofiSolo <sofa.galaxia@yandex.ru>
2022-09-04 18:20:06 +0300

    задание 1

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Домашняя работа № 7");
+        // ЗАДАНИЕ 1
+
+        int clientOs= 1;
+        if (clientOs==0 ){
+            System.out.println("Установите версию приложения для iOS по ссылке ");}
+        else {
+            System.out.println( " Установите версию приложения для Android по ссылке");}
+
+        //ЗАДАНИЕ 2
+
+
+    }
+
+
+}
